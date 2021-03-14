521c39b2e731c433ef5b8979aac180497a34898f
Alexandre Gérault <alexandre@hexium.io>
2021-03-14 11:48:10 +0100

    feat(UI): add handler to button

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -4,12 +4,13 @@ import style from '../../styles/UI/Button.module.css';
 
 interface Props {
   type?: 'button' | 'submit' | 'reset';
+  onClick?: () => void;
 }
 
-const Button: FC<Props> = ({ children, type = 'button' }) => {
+const Button: FC<Props> = ({ children, type = 'button', onClick }) => {
   return (
     // eslint-disable-next-line react/button-has-type
-    <button type={type} className={style.button}>
+    <button type={type} className={style.button} onClick={onClick}>
       {children}
     </button>
   );
