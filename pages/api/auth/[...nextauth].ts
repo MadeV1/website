2908e89f86301226b1a955db995c76f450488918
Alexandre Gérault <alexandre@hexium.io>
2021-04-08 10:15:11 +0200

    feat(registration): install next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+import { User } from '@/types/Fetch/User';
+import api from '@/utils/api';
+
+export default NextAuth({
+  providers: [
+    Providers.Credentials({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Adresse mail', type: 'email', placeholder: 'Votre adresse mail' },
+        password: { label: 'Mot de passe', type: 'password', placeholder: 'Votre mot de passe' },
+      },
+      async authorize(credentials) {
+        try {
+          const user = await api.post<User>('/login', { email: credentials.email, password: credentials.password });
+          if (user) {
+            return user.data;
+          }
+          return null;
+        } catch (error) {
+          if (error.response) {
+            return null;
+          }
+        }
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/connexion',
+  },
+});
