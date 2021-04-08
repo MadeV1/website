fbd2a8b96a434718127185ee683fb0ddcd06124c
Alexandre Gérault <alexandre@hexium.io>
2021-04-08 11:11:00 +0200

    fix(project): use optional operator and picsum for development process

--- components/UI/ProjectCard.tsx
+++ components/UI/ProjectCard.tsx
@@ -10,16 +10,16 @@ interface Props {
 }
 
 const ProjectCard: FC<Props> = ({ project }) => {
-  const { id, name, category, body: content, imageUrl, difficulty } = project;
+  const { id, name, category, body: content, difficulty } = project;
 
   return (
     <article className={styles.card}>
       <div className={styles.hero}>
-        <img src={imageUrl} alt={`Aperçu du projet ${name}`} />
+        <img src="https://picsum.photos/200/300" alt={`Aperçu du projet ${name}`} />
       </div>
       <header>
         <h1 className={`title ${styles.title}`}>{name}</h1>
-        <p className={`subtitle ${styles.subtitle}`}>{category.name}</p>
+        <p className={`subtitle ${styles.subtitle}`}>{category?.name}</p>
       </header>
       <p className={`${styles.resume}`}>{`${content?.substr(0, 200)}...`}</p>
       <footer className={styles.actions}>
